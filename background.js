5c6a3400d45d946c25f3fddbe3fa5c28f1ba7cc9
ppatil002 <pratilvpatil2002@gmail.com>
2022-10-02 14:30:33 +0530

    Clip the selected image and redirect it to new tab

---
+++ background.js
@@ -0,0 +1,137 @@
+// Sends a single message to the content script(s) in the specified tab,
+// with an optional callback to run when a response is sent back.
+//$ The runtime.onMessage
+// event is fired in each content script running in the specified tab for the current extension.
+
+// Function that initiates the process of creating a custom area screenshot
+async function initiateCustomAreaScreenshot(currentTab, filename) {
+  // Hide scrollbar before capturing the tab to avoid showing the scrollbar in the selection area
+  // await sendMessageToToggleScrollbar("hideScrollbar", currentTab);
+  console.log("Background strated");
+  // Capture visible tab to draw the selection area over
+  await captureTab(200).then(async (createdScreenshot) => {
+    if (createdScreenshot) {
+      // Send message to the custom area content script to display overlay
+      chrome.tabs.sendMessage(
+        currentTab.id,
+        {
+          imageURI: createdScreenshot,
+          currentTab: currentTab,
+          filename: filename,
+          action: "createCustomAreaScreenshot",
+        },
+        (responseCallback) => {
+          if (responseCallback) {
+            console.log(
+              "Message has reached the recipient (content-custom-area.js): Sent message to content script to create an overlay to select a custom area to screenshot"
+            );
+          }
+        }
+      );
+    }
+  });
+}
+
+// Function to asynchronously capture the currently visible part of the active tab and return the screenshot
+async function captureTab(timeout) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      chrome.tabs.captureVisibleTab(
+        null,
+        { format: "png" },
+        async (dataURL) => {
+          if (dataURL) {
+            resolve(dataURL);
+          }
+        }
+      );
+    }, timeout);
+  });
+}
+
+// Function to listen to the custom area content script (content-custom-area.js) to send the custom area image to the new tab
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.data.action === "sendCustomAreaScreenshot") {
+    // Call sendImageToNewTab() with the new screenshot of the selected area
+    sendImageToNewTab(
+      message.data, //? data contains img url img width&height
+      message.currentTabId,
+      message.currentTabIndex,
+      message.filename
+    );
+
+    sendResponse(JSON.stringify(message, null, 4) || true);
+  }
+});
+
+// Function to send an image + additional information to a new tab
+async function sendImageToNewTab(
+  data,
+  currentTabId,
+  currentTabIndex,
+  filename
+) {
+  // Create new tab to place the created screenshot in
+  const createdTabPromise = createTab(currentTabId, currentTabIndex);
+
+  // Run when promise is fulfilled (content script on new tab loaded)
+  createdTabPromise.then((createdTab) => {
+    // Workaround to fix the bug where 2 tabs are created after selecting a custom area to screenshot
+    // TODO: Replace with better solution for the bug
+    // Go through all tabs matching the url and close everyone that doesn't match the id of the created tab & and the index is bigger than the current tab index + 2
+    chrome.tabs.query(
+      {
+        currentWindow: true,
+        url: "chrome-extension://dfofdengbpakahfhbfdoeicpecgbldco/screenshot_edit.html",
+      },
+      function (tabs) {
+        for (var i = 0; i < tabs.length; i++) {
+          if (
+            tabs[i].id !== createdTab.id &&
+            tabs[i].index > currentTabIndex + 1
+          ) {
+            chrome.tabs.remove(tabs[i].id);
+          }
+        }
+      }
+    );
+
+    // Add action and filename to data object
+    data.action = "sendImageToNewTab";
+    data.filename = filename;
+
+    // Send the image + additional information to the newly created tab
+    chrome.tabs.sendMessage(createdTab.id, data, (responseCallback) => {
+      if (responseCallback) {
+        console.log(
+          "Image clipped and send from content to background(Open New Tab)"
+        );
+
+        // Manually change to the newly created tab
+        chrome.tabs.update(createdTab.id, { active: true, highlighted: true });
+      }
+    });
+  });
+}
+
+// Function to asynchronously create a new tab and return created tab after its content script is loaded
+function createTab(currentTabId, currentTabIndex) {
+  return new Promise((resolve) => {
+    chrome.tabs.create(
+      {
+        active: false,
+        url: "screenshot_edit.html",
+        openerTabId: currentTabId,
+        index: currentTabIndex + 1,
+      },
+      async (createdTab) => {
+        chrome.tabs.onUpdated.addListener(function listener(tabId, info) {
+          if (info.status === "complete" && tabId === createdTab.id) {
+            chrome.tabs.onUpdated.removeListener(listener);
+            resolve(createdTab);
+          }
+        });
+      }
+    );
+  });
+}
---
+++ content-custom-area.js
@@ -0,0 +1,249 @@
+"use strict";
+
+// Constants
+const overlayId = "snapper-overlay";
+const closeButtonId = "snapper-close-button";
+const canvasId = "snapper-canvas";
+const clippedCanvasId = "snapper-clipped-canvas";
+
+const allVideosOnPage = document.querySelectorAll("video");
+const allVideosPlaying = [];
+
+const windowInnerWidthString = window.innerWidth.toString();
+const windowInnerHeightString = window.innerHeight.toString();
+
+var currentTab;
+var filename;
+
+// Variables for the canvas and drawing the selection area in the canvas
+var overlay;
+var canvas;
+var canvasContext;
+var clippedCanvas;
+var clippedCanvasContext;
+var isDrawing = false;
+var offsetX;
+var offsetY;
+var startX;
+var startY;
+var mouseX;
+var mouseY;
+
+var visibleTabImageURI;
+var clippedImageURI;
+
+// Listen to the message sent by the background script to instantiate the overlay
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === "createCustomAreaScreenshot") {
+    // Assign sent variables to global variables
+    visibleTabImageURI = message.imageURI;
+    currentTab = message.currentTab;
+    filename = message.filename;
+
+    // If no overlay, close button and canvas exists, create them
+    if (
+      !(
+        document.getElementById(overlayId) &&
+        document.getElementById(closeButtonId) &&
+        document.getElementById(canvasId)
+      )
+    ) {
+      // Create canvas showing a screenshot of the page
+      canvas = createDrawableCanvas();
+      // // Create overlay over canvas
+      // overlay = createOverlayElement();
+      // // Add close button to overlay
+      addCloseButton();
+    }
+
+    sendResponse(JSON.stringify(message, null, 4) || true);
+
+    return true;
+  }
+});
+
+// Function to create a canvas element to draw a rectangular area over it to screenshot
+function createDrawableCanvas() {
+  // Hide scrollbar before canvas is created to avoid the image being shifted to the left by the width of the scrollbar
+  document.body.style.overflow = "hidden";
+
+  // Create canvas element
+  const mainCanvas = createCanvas(
+    canvasId,
+    `${windowInnerWidthString}px`,
+    `${windowInnerHeightString}px`
+  );
+
+  // Get canvas context to draw into the canvas
+  canvasContext = mainCanvas.getContext("2d");
+
+  // Workaround to create an image element and setting the source to the visibleTabImageURI to pass it into drawImage()
+  const image = new Image();
+  image.src = visibleTabImageURI;
+  image.onload = () =>
+    canvasContext.drawImage(image, 0, 0, window.innerWidth, window.innerHeight);
+
+  // Get any offset of the canvas to subtract during the area selection
+  offsetX = mainCanvas.offsetLeft;
+  offsetY = mainCanvas.offsetTop;
+
+  // Get current mouse position on mouse down as a starting position
+  mainCanvas.onmousedown = (e) => {
+    isDrawing = true;
+    startX = parseInt(e.clientX - offsetX);
+    startY = parseInt(e.clientY - offsetY);
+  };
+
+  // Set isDrawing to false to not register mouse movements while not holding mouse down
+  mainCanvas.onmouseup = (e) => {
+    isDrawing = false;
+    console.log("x=", startX, mouseX);
+    console.log("y=", startY, mouseY);
+
+    // Clip canvas to selected area and turn clipped canvas into image to pass to new tab
+    clipCanvasAndCreateImage();
+  };
+
+  // Get current and starting mouse position to draw a rectangle over canvas
+  mainCanvas.onmousemove = (e) => {
+    if (isDrawing) {
+      mouseX = parseInt(e.clientX - offsetX);
+      mouseY = parseInt(e.clientY - offsetY);
+
+      // Clear canvas and draw a new rectangle on every mouse move
+      canvasContext.clearRect(0, 0, mainCanvas.width, mainCanvas.height);
+      canvasContext.strokeRect(
+        startX,
+        startY,
+        mouseX - startX,
+        mouseY - startY,
+        drawRectangle(
+          startX,
+          startY,
+          mouseX - startX,
+          mouseY - startY,
+          mainCanvas
+        )
+      );
+      //   canvasContext.strokeStyle = "black";
+    }
+  };
+
+  document.body.appendChild(mainCanvas);
+
+  return mainCanvas;
+}
+
+function drawRectangle(x, y, width, height, c) {
+  let ctx = c.getContext("2d");
+  ctx.globalAlpha = 0.5;
+
+  // set fill and stroke styles
+  ctx.fillStyle = "yellow";
+  ctx.strokeStyle = "black";
+
+  // draw a rectangle with fill and stroke
+  ctx.fillRect(x, y, width, height);
+  ctx.strokeRect(x, y, width, height);
+}
+
+// Function that dynamically creates a canvas element with a given width,height and id and returns it
+function createCanvas(id, width, height) {
+  // Create canvas element
+  const createdCanvas = document.createElement("canvas");
+
+  // Set canvas properties
+  createdCanvas.setAttribute("id", id);
+  createdCanvas.setAttribute("width", width);
+  createdCanvas.setAttribute("height", height);
+
+  return createdCanvas;
+}
+
+// Function that clips the canvas to the selected area and converts it into a base64 image
+function clipCanvasAndCreateImage() {
+  // Create another canvas that is as big as the selected area
+  clippedCanvas = createCanvas(
+    clippedCanvasId,
+    `${mouseX - startX}px`,
+    `${mouseY - startY}px`
+  );
+
+  // Get canvas context to draw the clipped image into the canvas
+  clippedCanvasContext = clippedCanvas.getContext("2d");
+
+  // Set the visible tab image from before as the source of the image for the new canvas
+  const image = new Image();
+  image.src = visibleTabImageURI;
+
+  // Draw the visible content image into the new canvas, BUT clip it to the before selected area
+  image.onload = () => {
+    clippedCanvasContext.drawImage(
+      image, // load image into the canvas
+      startX, // x position (upper left corner) of the selected area
+      startY, // y position (upper left corner) of the selected area
+      mouseX - startX, //!width of the selected area Add +10
+      mouseY - startY, //! height of the selected area
+      0, // x position of where to place the clipped image in the canvas
+      0, // y position of where to place the clipped image in the canvas
+      mouseX - startX, // width of the screenshot in the canvas (aspect ratio)
+      mouseY - startY
+    ); // height of the screenshot in the canvas (aspect ratio)
+
+    // Turn clipped canvas into image
+    clippedImageURI = clippedCanvas.toDataURL("image/png"); //!.png .jpeg
+
+    // Create data object including everything needed to show the image on the new tab
+    var data = {
+      image: clippedImageURI,
+      width: window.innerWidth, //!width=mouseX-startX
+      height: window.innerHeight,
+      devicePixelRatio: window.devicePixelRatio,
+      action: "sendCustomAreaScreenshot",
+    };
+
+    // Send a message to the background script to send the custom area screenshot to the new tab
+    chrome.runtime.sendMessage(
+      {
+        data: data,
+        currentTabId: currentTab.id,
+        currentTabIndex: currentTab.index,
+        filename: filename,
+      },
+      (responseCallback) => {
+        if (responseCallback) {
+          console.log(
+            "Message has reached the recipient (background.js): send custom area screenshot to the new tab"
+          );
+        }
+
+        return true;
+      }
+    );
+  };
+}
+
+function addCloseButton() {
+  // Create button element to close overlay when clicked
+  const closeButton = document.createElement("button");
+
+  closeButton.textContent = "×";
+
+  closeButton.setAttribute("id", closeButtonId);
+
+  // Add on click event listener to the button
+  closeButton.onclick = () => {
+    // Remove all added elements from the body
+    document.body.removeChild(canvas);
+    // document.body.removeChild(overlay);
+    document.body.removeChild(closeButton);
+
+    // Unpause all paused videos that were playing before
+    allVideosPlaying.forEach((vid) => vid.play());
+
+    // Enable scrolling again
+    document.body.style.overflow = "visible";
+  };
+
+  document.body.appendChild(closeButton);
+}
